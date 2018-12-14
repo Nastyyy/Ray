cba70632d4ea51acd7009207e2de0522bb01c1c3
Nastyyy <nerorazerx@gmail.com>
2018-12-13 16:09:09 -0800

    Inserted underscores in json tags for ItemHistogram and Listing struct

--- ItemHistogram.go
+++ ItemHistogram.go
@@ -5,14 +5,14 @@ package main
 // It also represents the data stored in the database.
 type ItemHistogram struct {
 	UID               string    `json:"uid,omitempty"`
-	BuyOrderListings  int       `json:"buyorderlistings,omitempty"`
-	SellOrderListings int       `json:"sellorderlisitings,omitempty"`
-	BuyOrderGraph     []Listing `json:"buyordergraph,omitempty"`
-	SellOrderGraph    []Listing `json:"sellordergraph,omitempty"`
+	BuyOrderListings  int       `json:"buy_order_listings,omitempty"`
+	SellOrderListings int       `json:"sell_order_lisitings,omitempty"`
+	BuyOrderGraph     []Listing `json:"buy_order_graph,omitempty"`
+	SellOrderGraph    []Listing `json:"sell_order_graph,omitempty"`
 }
 
 // Listing is a representation of each buy/sell listing from a histogram of a given item
 type Listing struct {
 	Price         float64 `json:"price,omitempty"`
-	AmountAtPrice int     `json:"amountatprice,omitempty"`
+	AmountAtPrice int     `json:"amount_at_price,omitempty"`
 }
